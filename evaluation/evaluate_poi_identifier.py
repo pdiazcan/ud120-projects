0ccc0463711b98e82a66235e25fda8dcce8ca078
pdiazcan <pdiazcan@gmail.com>
2017-09-03 21:46:29 +0200

    Add Validation exercises

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -21,11 +21,32 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson14_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
 
 
 ### your code goes here 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.cross_validation import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split(features, labels, 
+                                                    test_size=0.3,
+                                                    random_state=42)
 
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, pred))
+print("Precision:", precision_score(y_test, pred))
+print("Recall:", recall_score(y_test, pred))
+print("Num of POI predicted:", sum(pred))
+print("Num of POI in reality:", sum(y_test))
+print("Total test examples:", len(pred))
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+#How many true positives are there?
